64bb70462fc3738f5639ad6f88c11b1cd12154ff
matthew <matt.feinberg@gmail.com>
2019-04-24 19:34:09 -0500

    no real change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,11 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host: "ec2-18-188-224-91.us-east-2.compute.amazonaws.com/",
+      host: "ec2-18-188-224-91.us-east-2.compute.amazonaws.com",
       key: "~/.ssh/sdcDesc1.pem",
       ref: "origin/master",
       repo: "https://github.com/threetexansandacanadian/NavBar.git",
-      path: "/home/ubuntu/navBar",
+      path: "/home/ubuntu/NavigationBar",
       "post-deploy":
         "npm install && npm run build-one && pm2 startOrRestart ecosystem.config.js"
     }
