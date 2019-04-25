daa915cc33ff03cf9842f53096dd37e6640ef6bd
matthew <matt.feinberg@gmail.com>
2019-04-25 11:26:47 -0500

    better

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,10 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host: "ec2-18-188-224-91.us-east-2.compute.amazonaws.com",
+      host: "http://ec2-18-216-43-36.us-east-2.compute.amazonaws.com/",
       key: "~/.ssh/sdcDesc1.pem",
       ref: "origin/master",
-      // repo: "https://github.com/threetexansandacanadian/NavBar.git",
-      repo: "https://github.com/most-amaziin/NavigationBar.git"
+      repo: "https://github.com/most-amaziin/NavigationBar.git",
       path: "/home/ubuntu/NavigationBar",
       "post-deploy":
         "npm install && npm run build-one && pm2 startOrRestart ecosystem.config.js"
